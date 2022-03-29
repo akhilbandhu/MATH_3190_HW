70c2168b712e8f0762da3d4ab412f879202b2c6d
akhilbandhu <akhilbandhu@gmail.com>
2022-03-28 18:23:10 -0600

    homework 5 work added

---
+++ homework5/homework5_anand.R
@@ -0,0 +1,14 @@
+# Homework 5
+# This script contains all the ggplot and other functions related to the homework
+
+library(ggplot2)
+
+# Question 1 
+# Using ggplot, plot the Poisson pmf for k=0,1,....,10 when lambda=5.
+ggplot(
+  transform(
+    data.frame(x=c(0:10)), 
+    y=dpois(x, 5)), aes(x, y)) +
+  geom_line()
+  
+
