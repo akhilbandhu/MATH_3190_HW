f0923276553d515cc6f556656bd64f0536ca6474
akhilbandhu <akhilbandhu@gmail.com>
2022-03-31 17:32:31 -0600

    homework 5 completed

--- homework5/homework5_anand.R
+++ homework5/homework5_anand.R
@@ -14,4 +14,20 @@ ggplot(
 
 # Question 2
 # Using ggplot, plot the pdf for an individual xi given θ = 0.5 and also for θ = 5
+theta_function <- function(x, theta) {
+  return(prod(theta*x^(theta-1)))     
+}
 
+theta <- c(1:10)/5
+theta_values<- NULL
+for(i in theta) {
+  theta_values <- c(theta_values, theta_function(c(1:9)*0.1, i)) 
+}
+
+df <- data.frame(y = theta_values, x = theta)
+
+ggplot(
+    df,
+    aes(x, y)
+  ) + 
+  geom_line()
